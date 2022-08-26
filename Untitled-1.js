7f00d96398a5849712fa73a69817d8cd77095d35
User <alexandrovsergeycurses.gmail.com>
2022-08-26 15:07:30 +0300

    Untitled-1.js

---
+++ Untitled-1.js
@@ -0,0 +1,33 @@
+
+ 
+function hui() {
+    var a = 10 ;
+    var b = 20 ;
+    var rev = a + b ;
+    
+    console.log(rev)
+    }
+
+    hui()
+     
+    
+    
+
+    function addTwoNums(a,b) {
+        var c = a + b ;
+        console.log(c)
+    }
+
+
+addTwoNums(2,2)
+addTwoNums(6,6)
+addTwoNums(7,6)
+ 
+function hello() {
+    console.log("Hello World!")
+}
+
+hello()
+
+var train1 = [ "banan", "water", "zero"]
+console.log(train1[2])
